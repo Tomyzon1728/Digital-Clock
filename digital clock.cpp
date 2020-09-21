6ce57e6cd403a6922999477bb572bef57001edbf
SathyaRam <50044265+SathyaseelanRamachandran@users.noreply.github.com>
2020-09-21 20:00:52 +0800

    Update digital clock.cpp
    The class constructor used to build the digital clock. Please have a look as this code will increase the program effeciency and effectiveness

--- digital clock.cpp
+++ digital clock.cpp
@@ -33,6 +33,65 @@ int main(){
 
 	
 	
+	//Using the class.cpp class header to define and develop digital clock program
+	#include <iostream>  // this is needed because of
+using namespace std; // the "cout" below.
+
+//The Clock class definition starts here. 
+// Again, given here as one unit for clarity where 
+// as normally this definition would be divided 
+// into two parts: specification and implementation
+// and written in separate files.  
+
+class Clock 
+{ 
+  private:
+    //declarations of data members that are private
+    int hour;          //an hour in the range 1 - 12
+    int minute,        //a minute in the range 0 - 59
+    int second;        //a second in the range 0 - 59
+    string meridian;   //is the time AM or PM
+
+  public: 
+    // publically accessible methods, i.e., the public interface 
+
+    //Set the clock to the specified time 
+    void setClock (int h, int m, int s, string mer)
+    {
+       hour = h;
+       minute = m;
+       second = s; 
+       meridian = mer;
+    }
+
+    //Display the time in standard notation
+    void displayStandard()
+    {
+      cout << hour<<':'<<minute<<':'<<second<<' ' << meridian ;
+    }
+
+    //Display the time in military notation
+    void displayMilitary()
+    {
+      int militaryHour = hour;
+
+      //if it is pm and it isn't 12:00 noon, then add 
+      //12 hours to the time
+      if (meridian == "PM" && hour != 12)
+          militaryHour += 12;
+
+      //if it is 12:00 midnight, then subtract 12 hours
+      if (meridian == "AM" && hour == 12)
+          militaryHour -= 12;
+
+      cout << militaryHour << ':' << minute <<  ':' << second;		  
+    }
+
+};
+//The Clock class definition ends with the closing brace and semicolon.
+
+	
+	
 	
 	
 	
